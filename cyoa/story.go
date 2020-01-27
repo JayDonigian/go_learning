b941f35fd5be9039f7b26c5155cfd44b45d18954
Jay Donigian <jaydonigian@github.com>
2020-01-27 14:52:06 -0700

    Pushing up to check formatting.

--- cyoa/story.go
+++ cyoa/story.go
@@ -1,1 +1,20 @@
 package cyoa
+
+// CYOA is the text representation of the story. It is read in from a JSON file.
+type CYOA struct {
+	Name      string
+	Title     string
+	Adventure []string
+	Options   []Option
+}
+
+// An Option is the representation of the end of a Story
+type Option struct {
+	Text string
+	Arc  string
+}
+
+// New creates a CYOA based on a given JSON file.
+func New() {
+
+}
